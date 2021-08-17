0a8ed197521473985f2655caee749b96fceb04be
MohammadHMansour <mdmansour@gmail.com>
2021-08-18 00:30:31 +0300

    created query to get short urls of the user Relates #22

--- server/database/queries/getShortUrl.js
+++ server/database/queries/getShortUrl.js
@@ -1,11 +1,11 @@
 const connection = require('../config/connection');
 
-const checkUrlQuery = (shortUrl) => {
+const getShortUrlQuery = (userName) => {
   const sqlScript = {
-    text: 'SELECT SHORT_URL FROM URLS WHERE SHORT_URL = $1',
-    values: [shortUrl],
+    text: 'SELECT SHORT_URL FROM URLS WHERE USER_NAME = $1',
+    values: [userName],
   };
   return connection.query(sqlScript).then((data) => data.rows.length !== 0);
 };
 
-module.exports = checkUrlQuery;
+module.exports = getShortUrlQuery;
--- server/database/queries/index.js
+++ server/database/queries/index.js
@@ -1,3 +1,9 @@
 const addUserQuery = require('./addUser');
+const addUrlQuery = require('./addUrl');
+const checkUrlQuery = require('./checkUrl');
+const checkUserUrlQuery = require('./checkUserUrl');
+const getShortUrlQuery = require('./getShortUrl');
 
-module.exports = { addUserQuery };
+module.exports = {
+  addUserQuery, addUrlQuery, checkUrlQuery, checkUserUrlQuery, getShortUrlQuery,
+};
