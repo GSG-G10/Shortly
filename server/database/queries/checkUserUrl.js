e88aeb4cc9e7c7964764e61ea5705806b727083a
MohammadHMansour <mdmansour@gmail.com>
2021-08-18 02:43:52 +0300

    created a function to shorten the original url and return the shortend url to the client Relates #30

--- server/database/queries/checkUserUrl.js
+++ server/database/queries/checkUserUrl.js
@@ -2,10 +2,10 @@ const connection = require('../config/connection');
 
 const checkUserUrlQuery = (userName, url) => {
   const sqlScript = {
-    text: 'SELECT USER_NAME FROM URLS WHERE LONG_URL = $2 AND USER_NAME = $1',
+    text: 'SELECT SHORT_URL FROM URLS WHERE LONG_URL = $2 AND USER_NAME = $1',
     values: [userName, url],
   };
-  return connection.query(sqlScript).then((data) => data.rows.length !== 0);
+  return connection.query(sqlScript).then((data) => data.rows);
 };
 
 module.exports = checkUserUrlQuery;
