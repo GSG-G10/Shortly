01b94234d89d50e196752541c1f25ba3fad76924
MohammadHMansour <mdmansour@gmail.com>
2021-08-18 00:04:03 +0300

    created a query to chek if the url is in the user account Relates #20

---
+++ server/database/queries/checkUserUrl.js
@@ -0,0 +1,11 @@
+const connection = require('../config/connection');
+
+const addUserQuery = (userName, url) => {
+  const sqlScript = {
+    text: 'SELECT USER_NAME FROM URLS WHERE LONG_URL = $2 AND USER_NAME = $1',
+    values: [userName, url],
+  };
+  return connection.query(sqlScript).then((data) => data.rows.length !== 0);
+};
+
+module.exports = addUserQuery;
